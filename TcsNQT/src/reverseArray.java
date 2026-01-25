3e1444a1406cdb4aa5aa5846cf84cd2f7f064743
venkatyallankishiva <yallankivenkatshiva2009@gmail.com>
2026-01-25 12:25:58 +0530

    4.You are given an array. The task is to reverse the array and print it.

---
+++ TcsNQT/src/reverseArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class reverseArray {
+    public static void reverse(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int size=sc.nextInt();
+        System.out.println("enter the elements of array");
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int left=0,right=arr.length-1;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        for(int a:arr){
+            System.out.print(a+" ");
+        }
+    }
+}
