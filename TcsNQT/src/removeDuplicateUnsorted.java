a3f509dc275d069dba7619faa3d00d814f653a52
venkatyallankishiva <yallankivenkatshiva2009@gmail.com>
2026-01-26 14:54:25 +0530

    12.Given an unsorted array, remove duplicates from the array.

---
+++ TcsNQT/src/removeDuplicateUnsorted.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class removeDuplicateUnsorted {
+    public static void remove(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int size=sc.nextInt();
+        System.out.println("enter the elements of array");
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<size;i++){
+            if(map.containsKey(arr[i])){
+                int count=map.get(arr[i]);
+                map.put(arr[i],count+1);
+            }
+            else {
+                map.put(arr[i],1);
+            }
+        }
+
+        System.out.println(map.keySet());
+    }
+}
